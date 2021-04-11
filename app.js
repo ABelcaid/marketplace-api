f3ff7f09af68b365ad72a32b3942a79c037fae44
belcaid abderrahim <belcaidna@gmail.com>
2021-04-11 16:05:30 +0000

    add core origin

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 
 app.use(cors({
-  origin : ['http://localhost:3000'],
+  origin : ['http://localhost:3000',process.env.CORS_ORIGIN],
   credentials : true
 }));
 
