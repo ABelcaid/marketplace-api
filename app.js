4d019edcf1c76a59e8221aaf3954c97fa99084fe
belcaid abderrahim <belcaidna@gmail.com>
2021-04-12 09:34:38 +0000

    add cors

--- app.js
+++ app.js
@@ -14,13 +14,18 @@ const port = process.env.PORT || 8080;
 
 app.use(express.json());
 
-app.use(cors({
-  origin : ['http://localhost:3000','https://marketplace-lupin.herokuapp.com:3000'],
-  credentials : true
-}));
+// app.use(cors({
+//   origin : ['http://localhost:3000',':3000'],
+//   credentials : true
+// }));
 
 
-
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'https://marketplace-lupin.herokuapp.com');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 
 app.use(express.urlencoded({ extended: true }));
